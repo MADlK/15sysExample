2d32e4393fa0c6d036c5ed74c28068ff18fcd394
unknown <you@example.com>
2026-04-09 13:24:01 +0500

    123

--- Windows Sockets/WinSocketClient/WinSocketClient/WinSocketClient.cpp
+++ Windows Sockets/WinSocketClient/WinSocketClient/WinSocketClient.cpp
@@ -1,45 +1,45 @@
-﻿#define WIN32_LEAN_AND_MEAN // Отключаем лишние части Windows.h
-#include <Windows.h>        // Базовые функции Windows API
-#include <winsock2.h>       // Сетевые функции Winsock 2
-#include <WS2tcpip.h>       // Функции для работы с адресами
-#include <iphlpapi.h>       // (Оставлен как в оригинале)
-#include <iostream>         // Ввод/вывод в консоль
+﻿#define WIN32_LEAN_AND_MEAN 
+#include <Windows.h>        
+#include <winsock2.h>       
+#include <WS2tcpip.h>       
+#include <iphlpapi.h>       
+#include <iostream>         
 #include <string>         
 
-#pragma comment(lib, "Ws2_32.lib") // Линкуем библиотеку Winsock
+#pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
 
 
-bool online = true;      // Глобальный флаг: управляет работой потока приёма
-SOCKET ClientSocekt = INVALID_SOCKET; // Глобальный сокет: передаётся потоку для чтения
+bool online = true;      
+SOCKET ClientSocekt = INVALID_SOCKET; 
 
 
 
-// Поток WinAPI для асинхронного приёма сообщений от сервера
+
 DWORD WINAPI RecvThread(LPVOID) 
 {
     setlocale(0, "ru");
-    const int buflen = 512; // Размер буфера
-    char recbuf[buflen];    // Буфер данных
-    int iResult;            // Результат recv
+    const int buflen = 512; 
+    char recbuf[buflen];    
+    int iResult;            
 
     while (online)
-    { // Пока клиент не закрылся
-        iResult = recv(ClientSocekt, recbuf, buflen - 1, 0); // Блокируем поток, ждём данные
+    {
+        iResult = recv(ClientSocekt, recbuf, buflen - 1, 0); 
         if (iResult > 0)
-        { // Если данные пришли
-            recbuf[iResult] = '\0'; // Закрываем строку
-            cout << recbuf; // Выводим с новой строки
-            //cout.flush();           // Принудительно выводим в консоль
+        {
+            recbuf[iResult] = '\0';
+            cout << recbuf; 
+            
         }
         else if (iResult == 0) 
-        { // Сервер закрыл соединение
+        { 
             cout << "\n[SERVER] сервер закрыл соединение" << endl;
             break;
         }
         else 
-        { // Ошибка сети
+        { 
             if (online)
             {
                 cout << "\n[ERROR] recv: " << WSAGetLastError() << endl; 
@@ -56,7 +56,7 @@ int main(int argc, char* argv[]) {
  
 
     WSAData wsaData;
-    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); // Инициализация Winsock
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); 
     if (iResult != 0)
     {
         cout << "WSAStartup: " << iResult << endl;
@@ -65,12 +65,12 @@ int main(int argc, char* argv[]) {
 
     struct addrinfo* result = NULL, * ptr = NULL, hints;
     ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;       // IPv4 или IPv6
-    hints.ai_socktype = SOCK_STREAM;   // TCP
+    hints.ai_family = AF_UNSPEC;       
+    hints.ai_socktype = SOCK_STREAM;   
     hints.ai_protocol = IPPROTO_TCP;
 
 #define DEFAULT_PORT "27015"
-    iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result); // Преобразуем IP
+    iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
     if (iResult != 0)
     {
         cout << "getaddrinfo error: " << iResult << endl;
@@ -80,7 +80,7 @@ int main(int argc, char* argv[]) {
 
     SOCKET ConnectSocket = INVALID_SOCKET;
     ptr = result;
-    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol); // Создаём сокет
+    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol); 
     if (ConnectSocket == INVALID_SOCKET) 
     { 
         cout << "socket(): " << WSAGetLastError() << endl;
@@ -88,37 +88,39 @@ int main(int argc, char* argv[]) {
         WSACleanup();
         return 1; }
 
-    iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen); // Подключаемся
+    iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen); 
     if (iResult == SOCKET_ERROR) 
     {
         cout << "нет соединения: " << WSAGetLastError() << endl;
-        closesocket(ConnectSocket); freeaddrinfo(result);
+        closesocket(ConnectSocket); 
+        freeaddrinfo(result);
         WSACleanup();
         return 1;
     }
-    freeaddrinfo(result); // Освобождаем память
+    freeaddrinfo(result); 
 
     ClientSocekt = ConnectSocket;
-    online = true; // Передаём сокет потоку
+    online = true; 
 
     const int buflen = 512;
     char sendbuf[buflen] = "";
     char namebuf[64] = "";
 
     cout << "Введите имя ";
-    cin.getline(namebuf, sizeof(namebuf)); // Читаем имя
+    cin.getline(namebuf, sizeof(namebuf)); 
     
 
-    iResult = send(ConnectSocket, namebuf, strlen(namebuf), 0); // Отправляем имя
+    iResult = send(ConnectSocket, namebuf, strlen(namebuf), 0); 
     if (iResult == SOCKET_ERROR) 
     {
-        cout << "Ошибка: " << WSAGetLastError() << endl; closesocket(ConnectSocket);
+        cout << "Ошибка: " << WSAGetLastError() << endl;
+        closesocket(ConnectSocket);
         WSACleanup();
         return 1; 
     }
 
     char ack[4] = "";
-    iResult = recv(ConnectSocket, ack, sizeof(ack) - 1, 0); // Ждём "OK" от сервера
+    iResult = recv(ConnectSocket, ack, sizeof(ack) - 1, 0); 
     if (iResult > 0)
     {
         ack[iResult] = '\0'; 
@@ -129,10 +131,10 @@ int main(int argc, char* argv[]) {
 
     }
 
-    HANDLE hRecv = CreateThread(NULL, 0, RecvThread, NULL, 0, NULL); // Запускаем поток приёма
+    HANDLE hRecv = CreateThread(NULL, 0, RecvThread, NULL, 0, NULL);
 
     while (cin.getline(sendbuf, buflen))
-    { // Читаем ввод с клавиатуры
+    {
         int len = strlen(sendbuf);
         if (len == 0)
             continue;
@@ -141,21 +143,24 @@ int main(int argc, char* argv[]) {
         {
             cout << "рассоединение..." << endl;
             break; 
-        } // Выход
-
-        iResult = send(ConnectSocket, sendbuf, len, 0); // Отправляем на сервер
+        } 
+        iResult = send(ConnectSocket, sendbuf, len, 0); 
         if (iResult == SOCKET_ERROR)
-        { cout << "\n[ERROR] send: " << WSAGetLastError() << endl;
-        break;
+        { 
+            cout << "\n[ERROR] send: " << WSAGetLastError() << endl;
+            break;
         }
     }
 
-    online = false; // Останавливаем поток
-    shutdown(ConnectSocket, SD_SEND); // Корректно закрываем отправку
+    online = false; 
+    shutdown(ConnectSocket, SD_SEND); 
     if (hRecv) 
     {
         WaitForSingleObject(hRecv, 1000); 
         CloseHandle(hRecv);
-    } // Ждём поток до 1 сек
-    closesocket(ConnectSocket); WSACleanup(); return 0;
+    }
+
+    closesocket(ConnectSocket);
+    WSACleanup();
+    return 0;
 }
--- Windows Sockets/WinSocketClient/WinSocketServer/WinSocketServer.cpp
+++ Windows Sockets/WinSocketClient/WinSocketServer/WinSocketServer.cpp
@@ -1,46 +1,31 @@
-﻿#define WIN32_LEAN_AND_MEAN // Отключаем лишние части Windows.h для ускорения компиляции
-#include <Windows.h>        // Подключаем базовые функции Windows API
-#include <winsock2.h>       // Подключаем библиотеку Winsock 2 для сетевого взаимодействия
-#include <WS2tcpip.h>       // Подключаем функции для работы с IPv4/IPv6 (getaddrinfo)
-#include <iphlpapi.h>       // Подключаем (оставлен как в оригинале, хотя не используется напрямую)
-#include <iostream>         // Подключаем стандартный ввод/вывод для cout/cin
+﻿#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>      
+#include <winsock2.h>     
+#include <WS2tcpip.h>     
+#include <iphlpapi.h>     
+#include <iostream>       
 #include <string>
 
-#pragma comment(lib, "Ws2_32.lib") // Линкуем библиотеку Winsock автоматически при компиляции
+#pragma comment(lib, "Ws2_32.lib")
 
-using namespace std; // Используем стандартное пространство имен для удобства
+using namespace std; 
 
-// Структура для хранения данных одного подключенного клиента
+
 struct ClientData {
-    SOCKET sock;       // Дескриптор сокета клиента (уникальный ID соединения)
-    char name[64];     // Имя клиента (максимум 63 символа + нуль-терминатор)
-    bool connected;    // Флаг активности: true = в сети, false = отключился
+    SOCKET sock;       
+    char name[64];     
+    bool connected;    
 };
 
-ClientData clients[32]; // Глобальный массив для хранения всех клиентов (лимит 32)
-int client_count = 0;   // Счётчик подключённых клиентов (для отладки/ограничений)
+ClientData clients[256]; 
+int client_count = 0;   
 
-//// Функция ручного вычисления длины строки (полная замена strlen)
-//int str_len(const char* s) {
-//    int len = 0;                      // Инициализируем счётчик длины
-//    while (s[len] != '\0') len++;     // Считаем символы до конца строки
-//    return len;                       // Возвращаем итоговую длину
-//}
 
-//// Функция ручного сравнения двух строк (полная замена strcmp)
-//bool str_equal(const char* a, const char* b) {
-//    int i = 0;                        // Индекс текущего символа
-//    while (a[i] != '\0' && b[i] != '\0') { // Пока обе строки не закончились
-//        if (a[i] != b[i]) return false;    // Если символы не совпали, строки разные
-//        i++;                               // Переходим к следующему символу
-//    }
-//    return a[i] == '\0' && b[i] == '\0'; // Строки равны, если обе закончились одновременно
-//}
 
 // всем кроме отправителя 
-void broadcast(const char* msg, SOCKET sender)
+void AllSend(const char* msg, SOCKET sender)
 {
-    for (int i = 0; i < 32; i++) 
+    for (int i = 0; i < 256; i++) 
     { 
         
         // в сети?                  не отправитель?              не инвалид   
@@ -51,85 +36,79 @@ void broadcast(const char* msg, SOCKET sender)
     }
 }
 
-//// Функция отправки приватного сообщения одному конкретному клиенту
-//void send_private(SOCKET target, const char* msg)
-//{
-//    send(target, msg, strlen(msg), 0); // Отправляем только целевому сокету
-//}
 
-// Функция потока WinAPI, которая выполняется отдельно для каждого клиента
+
+
+
 DWORD WINAPI Client(LPVOID param) 
 {
     setlocale(0, "ru");
-    ClientData* data = (ClientData*)param; // Преобразуем универсальный указатель в структуру клиента
-    SOCKET ClientSocket = data->sock;      // Получаем сокет для удобства
+    ClientData* data = (ClientData*)param; 
+    SOCKET ClientSocket = data->sock;      
 
-    const int buflen = 512;                // Максимальный размер буфера приёма
-    int iResult = 0;                       // Переменная для результата recv/send
-    int sendResult;                        // Переменная для результата send
-    char recvbuf[buflen] = "";             // Буфер для входящих сообщений (обнулён)
+    const int buflen = 512;                
+    int iResult = 0;                       
+    int sendResult;                        
+    char recvbuf[buflen] = "";             
 
-    char Name[64] = "";                    // Локальная копия имени клиента
-    int j = 0;                             // Вспомогательный индекс
-    while (j < 63 && data->name[j] != '\0') { // Копируем имя в локальный буфер
+    char Name[64] = "";                    
+    int j = 0;                             
+    while (j < 63 && data->name[j] != '\0')
+    { 
         Name[j] = data->name[j]; j++;
     }
-    Name[j] = '\0';                        // Гарантируем нуль-терминатор
+    //Name[j] = '\0';                        
 
-    send(ClientSocket, "OK", 2, 0);        // Отправляем подтверждение готовности чата
+    send(ClientSocket, "OK", 2, 0);        
 
     do
-    { // Цикл приёма сообщений, пока соединение живо
-        iResult = recv(ClientSocket, recvbuf, buflen - 1, 0); // Ждём данные от клиента
-        if (iResult > 0) {                  // Если принято >0 байт
-            recvbuf[iResult] = '\0';        // Ставим конец строки
+    { 
+        iResult = recv(ClientSocket, recvbuf, buflen - 1, 0); 
+        if (iResult > 0)
+        {                  
+            recvbuf[iResult] = '\0';        
 
-            if (recvbuf[0] == '/') {        // Если начинается с '/', это команда
-                cout << "[SERVER CMD] [" << Name << "] подключился: " << recvbuf << endl; // Логируем
+            if (recvbuf[0] == '/')
+            {
+                cout << "[SERVER CMD] [" << Name << "] подключился: " << recvbuf << endl; 
 
                 if (string(recvbuf) == "/users" || string(recvbuf) == "/Users") 
-                { // Если /users
-                    char user_list[2048] = "\n=== В сети ===\n"; // Буфер списка
-                    //int pos = strlen(user_list); // Текущая позиция в буфере
+                { 
+                    char user_list[2048] = "\n=== В сети ===\n"; 
+                    
 
-                    for (int i = 0; i < 32; i++)
-                    { // Перебираем всех клиентов
+                    for (int i = 0; i < 256; i++)
+                    { 
                         if (clients[i].connected)
-                        { // Если активен
-                            //user_list[pos++] = '-';
-                            //user_list[pos++] = ' '; // Маркер "- "
+                        { 
+                            
                             strcat_s(user_list, "- ");
                            
-                            //int k = 0;
-                            //while (clients[i].name[k] != '\0' && pos < 2047)
-                            //{
-                            //    user_list[pos++] = clients[i].name[k++];
-                            //} // Копируем имя
+                            
 
                             strcat_s(user_list, clients[i].name );
                             strcat_s(user_list, "\n");
 
-                            //user_list[pos++] = '\n'; // Перенос строки
+                   
                         }
                     }
-                    //user_list[pos] = '\0'; // Закрываем строку
+                   
                     strcat_s(user_list, "\0");
-                    send(ClientSocket, user_list, strlen(user_list), 0); // Шлём ТОЛЬКО запросившему
-                    //send_private(ClientSocket, user_list);
-                    continue; // Не рассылаем в общий чат
+                    send(ClientSocket, user_list, strlen(user_list), 0); 
+                    continue; 
                 }
-                continue; // Неизвестная команда: игнорируем, ждём следующую
+                continue; 
             }
 
-            cout << "[SERVER] [" << Name << "]: " << recvbuf << endl; // Логируем обычное сообщение
+            cout << "[SERVER] [" << Name << "]: " << recvbuf << endl;
 
             
-            char full_msg[700] = "[";  //сообщение
+            char full_msg[700] = "[";  
             strcat_s(full_msg, Name);
             strcat_s(full_msg, "]: ");
             strcat_s(full_msg, recvbuf);
             strcat_s(full_msg, "\n");
-            broadcast(full_msg, ClientSocket); // Рассылаем всем, кроме автора
+            AllSend(full_msg, ClientSocket); 
         }
         else if (iResult == 0)
         { 
@@ -137,14 +116,14 @@ DWORD WINAPI Client(LPVOID param)
             break;
         }
         else 
-        { // Ошибка сети или обрыв
+        { 
             cout << "[SERVER] recv: " << WSAGetLastError() << endl;
             break;
         }
     } while (iResult > 0);
 
-    for (int i = 0; i < 32; i++)
-    { // Помечаем клиента как отключенного
+    for (int i = 0; i < 256; i++)
+    {
         if (clients[i].sock == ClientSocket) 
         {
             clients[i].connected = false; 
@@ -152,23 +131,14 @@ DWORD WINAPI Client(LPVOID param)
         }
     }
 
-    char leave_msg[128] = "[SERVER] "; // Формируем уведомление об уходе
-    int pos = 9; int k = 0;
-    while (Name[k] != '\0' && pos < 127) 
-    {
-        leave_msg[pos++] = Name[k++]; 
-    }
-    const char* suffix = " left the chat\n";
-    k = 0; while (suffix[k] != '\0' && pos < 127)
-    { 
-        leave_msg[pos++] = suffix[k++];
-    }
-    leave_msg[pos] = '\0';
-    broadcast(leave_msg, ClientSocket);
+    char leave_msg[128] = "[SERVER] "; 
+    strcat_s(leave_msg, Name);
+    strcat_s(leave_msg, " вышел\n");
+    AllSend(leave_msg, ClientSocket);
 
-    closesocket(ClientSocket); // Закрываем сокет клиента
-    delete data;               // Освобождаем память кучи
-    return 0;                  // Завершаем поток
+    closesocket(ClientSocket); 
+    delete data;              
+    return 0;                 
 }
 
 
@@ -182,27 +152,27 @@ DWORD WINAPI Client(LPVOID param)
 
 int main(int argc, char* argv[])
 {
-    setlocale(0, "ru"); // Включаем русскую локаль
-    WSAData wsaData;    // Структура для Winsock
-    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); // Инициализируем Winsock 2.2
+    setlocale(0, "ru"); 
+    WSAData wsaData;   
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0)
     {
         cout << "WSAStartup failed: " << iResult << endl;
         return 1; 
     }
 
-#define DEFAULT_PORT "27015" // Порт для прослушивания
+#define DEFAULT_PORT "27015" 
     struct addrinfo*
         result = NULL,
         * ptr = NULL,
         hints;
-    ZeroMemory(&hints, sizeof(hints)); // Обнуляем структуру
-    hints.ai_family = AF_INET;         // Только IPv4
-    hints.ai_socktype = SOCK_STREAM;   // TCP
-    hints.ai_protocol = IPPROTO_TCP;   // Протокол TCP
-    hints.ai_flags = AI_PASSIVE;       // Для сервера (bind ко всем интерфейсам)
+    ZeroMemory(&hints, sizeof(hints)); 
+    hints.ai_family = AF_INET;         
+    hints.ai_socktype = SOCK_STREAM;   
+    hints.ai_protocol = IPPROTO_TCP;   
+    hints.ai_flags = AI_PASSIVE;       
 
-    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result); // Получаем бинарный адрес
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
     if (iResult != 0)
     {
         cout << "getaddrinfo error: " << iResult << endl; 
@@ -211,7 +181,7 @@ int main(int argc, char* argv[])
     }
 
     SOCKET ListenSocket = INVALID_SOCKET;
-    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol); // Создаём сокет
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol); 
     if (ListenSocket == INVALID_SOCKET) 
     {
         cout << "error at socket(): " << WSAGetLastError() << endl;
@@ -220,7 +190,7 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen); // Привязываем к IP:Port
+    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen); 
     if (iResult == SOCKET_ERROR)
     {
         cout << "bind failed: " << WSAGetLastError() << endl;
@@ -228,10 +198,10 @@ int main(int argc, char* argv[])
         closesocket(ListenSocket);
         WSACleanup(); return 1; }
 
-    freeaddrinfo(result); // Память адреса больше не нужна
+    freeaddrinfo(result);
 
     if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR)
-    { // Переводим в режим прослушивания
+    { 
         cout << "Listen: " << WSAGetLastError() << endl;
         closesocket(ListenSocket); 
         WSACleanup();
@@ -240,15 +210,15 @@ int main(int argc, char* argv[])
 
     
 
-    for (int i = 0; i < 32; i++)
-    { // Инициализируем массив клиентов
+    for (int i = 0; i < 256; i++)
+    { 
         clients[i].connected = false;
         clients[i].sock = INVALID_SOCKET;
     }
 
-    while (true) 
-    { // Бесконечный цикл ожидания подключений
-        SOCKET ClientSocket = accept(ListenSocket, NULL, NULL); // Блокируем, ждём клиента
+    while (true) //цикл подключения клиента
+    { 
+        SOCKET ClientSocket = accept(ListenSocket, NULL, NULL); 
         if (ClientSocket == INVALID_SOCKET)
         {
             cout << "accept failed: " << WSAGetLastError() << endl;
@@ -257,64 +227,50 @@ int main(int argc, char* argv[])
         cout << "[SERVER] Подключился новый пользователь" << endl;
 
         char recvbuf[512] = "";
-        iResult = recv(ClientSocket, recvbuf, 63, 0); // Читаем имя первым пакетом
-        char temp_name[64] = "Unknown";
+        iResult = recv(ClientSocket, recvbuf, 63, 0); // первым имя
+        char temp_name[64] = " ";
         if (iResult > 0)
         {
-            recvbuf[iResult] = '\0';
-            int len = (iResult < 63) ? iResult : 63;
-            int j = 0; while (j < len) { temp_name[j] = recvbuf[j]; j++; }
-            temp_name[len] = '\0';
+            strcat_s(temp_name, recvbuf);
         }
 
         int slot = -1;
-        for (int i = 0; i < 32; i++) 
+        for (int i = 0; i < 256; i++) 
         { 
             if (!clients[i].connected)
             {
-                slot = i; break; 
+                slot = i;
+                break; 
             }
-        } // Ищем свободный слот
+        } // Ищем свободный слот для подключения клиента
         
         if (slot == -1)
         {
-            send(ClientSocket, "Server full\n", 12, 0);
+            send(ClientSocket, "сервер заполнен\n", 20, 0);
             closesocket(ClientSocket); 
             continue; 
         }
 
-        ClientData* newData = new ClientData; // Выделяем память в куче
-        newData->sock = ClientSocket; newData->connected = true; // Заполняем
-        int j = 0;
-        while (j < 63 && temp_name[j] != '\0')
-        {
-            newData->name[j] = temp_name[j];
-            j++;
-        }
-        newData->name[j] = '\0';
+        ClientData* newData = new ClientData; 
+        newData->sock = ClientSocket;
+        newData->connected = true; 
+        strcat_s(newData->name, temp_name);
 
         clients[slot] = *newData;
-        client_count++; // Добавляем в массив
+        client_count++; 
 
         char join_msg[128] = "[SERVER] "; // Уведомление о входе
-        int pos = 9; j = 0; while (temp_name[j] != '\0' && pos < 127)
-        {
-            join_msg[pos++] = temp_name[j++];
-        }
-        const char* suffix = " joined the chat\n";
-        j = 0;
-        while (suffix[j] != '\0' && pos < 127)
-        {
-            join_msg[pos++] = suffix[j++]; 
-        }
-        join_msg[pos] = '\0';
-        broadcast(join_msg, ClientSocket);
+        strcat_s(join_msg,temp_name );
+        strcat_s(join_msg, "присоединился");
 
-        HANDLE hThread = CreateThread(NULL, 0, Client, newData, 0, NULL); // Создаём поток
-        if (hThread) CloseHandle(hThread); // Закрываем дескриптор, поток работает дальше
+        AllSend(join_msg, ClientSocket);//отправка всем
+
+        HANDLE hThread = CreateThread(NULL, 0, Client, newData, 0, NULL);
+        if (hThread)
+            CloseHandle(hThread);
     }
 
     closesocket(ListenSocket); 
     WSACleanup();
-    return 0; // (Недостижимо из-за while(true))
+    return 0; 
 }
